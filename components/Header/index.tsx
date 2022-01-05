cd8377ae69da9db62626891db166dd402b5699de
Keven Andrade <hirlandeer7@gmail.com>
2022-01-05 15:03:20 -0100

    Header done

---
+++ components/Header/index.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss';
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+export function Header(){
+    const currentdata = format(new Date(), 'EEEEEE, d MMMM', { 
+        locale: ptBR,
+    });
+
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.png" alt="Podcaster"/>
+            <p>O melhor para voce ouvir, sempre</p>
+
+            <span>{currentdata}</span>
+        </header>
+    );
+}
