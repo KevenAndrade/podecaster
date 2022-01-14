9398d0a70b6716a253723c06bef593783c96fa32
Keven Andrade <hirlandeer7@gmail.com>
2022-01-14 15:06:03 -0100

    Scroll na page

--- src/pages/episode/[nomeEp].tsx
+++ src/pages/episode/[nomeEp].tsx
@@ -30,32 +30,34 @@ export default function Episode({episode} : EpisodeProps ) {
     const router = useRouter();
 
     return (
-        <div className={styles.episode}>
-            <div className={styles.thumbnailContainer}>
-                <Link href="/">
+        <div className={styles.episodeContainer}>
+            <div className={styles.episode}>
+                <div className={styles.thumbnailContainer}>
+                    <Link href="/">
+                        <button type="button">
+                            <img src="/arrow-left.svg" alt="Voltar"/>
+                        </button>
+                    </Link>
+                    <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
                     <button type="button">
-                        <img src="/arrow-left.svg" alt="Voltar"/>
+                        <img src="/play.svg" alt="Tocar"/>
                     </button>
-                </Link>
-                <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
-                <button type="button">
-                    <img src="/play.svg" alt="Tocar"/>
-                </button>
+                </div>
+
+                <header>
+                    <h1>{episode.title}</h1>
+                    <span>{episode.members}</span>
+                    <span>{episode.publishedAt}</span>
+                    <span>{episode.durationAsString}</span>
+                </header>
+
+                <div 
+                    className={styles.description} 
+                    dangerouslySetInnerHTML={{__html: episode.description}} 
+                />
             </div>
-
-            <header>
-                <h1>{episode.title}</h1>
-                <span>{episode.members}</span>
-                <span>{episode.publishedAt}</span>
-                <span>{episode.durationAsString}</span>
-            </header>
-
-            <div 
-                className={styles.description} 
-                dangerouslySetInnerHTML={{__html: episode.description}} 
-            />
-             
         </div>
+        
     )
 }
 
