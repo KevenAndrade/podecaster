13436eb1584f3c60c13933d5aee813a1de77b334
Keven Andrade <hirlandeer7@gmail.com>
2022-01-21 12:03:15 -0100

    tag audio

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -47,6 +47,10 @@ export function Player(){
                     </div>
                     <span>00:00</span>
                 </div>
+                
+                { episode && (
+                    <audio src={episode.url} autoPlay/>
+                )}
 
                 <div className={styles.buttons}>
                     <button type="button" disabled={!episode}>
